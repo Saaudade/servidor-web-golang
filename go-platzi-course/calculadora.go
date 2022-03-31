9f7734e490d1fc80f3b915e2762cc0047a5a5658
Christian Alberto Tamayo Robayo <christian.tamayo@mercadolibre.com.co>
2022-03-31 13:28:45 -0500

    Structs y Receivers

--- go-platzi-course/calculadora.go
+++ go-platzi-course/calculadora.go
@@ -8,37 +8,46 @@ import (
 	"strings"
 )
 
-func main() {
+type calc struct {
+}
+
+func (calc) operate(input string, operator string) int {
+	inputClean := strings.Split(input, operator)
+	operator_1 := parsear(inputClean[0])
+	operator_2 := parsear(inputClean[1])
+	result := 0
+	switch operator {
+	case "+":
+		result = operator_1 + operator_2
+	case "-":
+		result = operator_1 - operator_2
+	case "*":
+		result = operator_1 * operator_2
+	case "/":
+		result = operator_1 / operator_2
+	default:
+		fmt.Printf("Operator %s is not available\n", operator)
+	}
+	return result
+}
+
+func parsear(input string) int {
+	operator, _ := strconv.Atoi(input)
+	return operator
+}
+
+func readInput() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	operacion := scanner.Text()
-	fmt.Println(operacion)
-	operador := "p"
+	return scanner.Text()
+}
 
-	valores := strings.Split(operacion, operador)
-	fmt.Println(valores)
-	fmt.Println(valores[0] + valores[1])
-	operador1, err1 := strconv.Atoi(valores[0])
+func main() {
+	input := readInput()
+	operator := readInput()
 
-	if err1 != nil {
-		fmt.Println(err1)
-	} else {
-		fmt.Println(operador1)
-	}
+	calculate := calc{}
+	result := calculate.operate(input, operator)
 
-	operador2, _ := strconv.Atoi(valores[1])
-
-	switch operador {
-	case "+":
-		fmt.Println(operador1 + operador2)
-	case "-":
-		fmt.Println(operador1 - operador2)
-	case "*":
-		fmt.Println(operador1 * operador2)
-	case "/":
-		fmt.Println(operador1 / operador2)
-	default:
-		fmt.Println(operador, "No esta soportado")
-	}
-
+	fmt.Println(result)
 }
