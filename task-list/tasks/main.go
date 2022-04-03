b72747f8f0da809a599fbe71cf63d8b8a5ff6867
Christian Alberto Tamayo Robayo <christian.tamayo@mercadolibre.com.co>
2022-04-03 18:03:27 -0500

    A task list was implemented
    A task list was implemented along with its function to add or delete a task

--- task-list/tasks/main.go
+++ task-list/tasks/main.go
@@ -20,13 +20,49 @@ func (t *task) setName(name string) {
 	t.name = name
 }
 
+type taskList struct {
+	tasks []*task
+}
+
+func (t *taskList) addToList(newTask *task) {
+	t.tasks = append(t.tasks, newTask)
+}
+
+func (t *taskList) deleteOfList(index int) {
+	t.tasks = append(t.tasks[:index], t.tasks[index+1:]...)
+}
+
 func main() {
-	t := &task{
+	t1 := &task{
 		name:        "Complete the go course",
 		description: "Complete the Platzi go course of in this week",
 	}
-	t.taskCompleted()
-	t.setName("Completed the go course x2")
-	t.setDescription("Completed the course very quickly")
-	fmt.Printf("%+v\n", t)
+
+	t2 := &task{
+		name:        "Complete the java course",
+		description: "Complete the Platzi java course of in this week",
+	}
+
+	t3 := &task{
+		name:        "Complete the python course",
+		description: "Complete the Platzi python course of in this week",
+	}
+
+	t4 := &task{
+		name:        "Complete the fury course",
+		description: "Complete the Platzi fury course of in this week",
+	}
+
+	list := &taskList{
+		tasks: []*task{
+			t1, t2, t3,
+		},
+	}
+
+	fmt.Println(list.tasks[1])
+	list.addToList(t4)
+	fmt.Println(len(list.tasks))
+	list.deleteOfList(1)
+	fmt.Println(list.tasks[1])
+	fmt.Println(len(list.tasks))
 }
