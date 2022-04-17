9feb5147dd87cc2ba1a1a56a6179fa71b8d21587
Christian Alberto Tamayo Robayo <christian.tamayo@mercadolibre.com.co>
2022-04-17 18:17:20 -0500

    Adding multiple middleware

--- web_server/middleware.go
+++ web_server/middleware.go
@@ -8,7 +8,7 @@ import (
 func CheckAuth() Middleware {
 	return func(f http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
-			flag := false
+			flag := true
 			fmt.Println("Checking Authentication")
 			if flag {
 				f(w, r)
